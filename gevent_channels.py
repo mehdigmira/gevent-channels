7bc683b54a170a02b9a1f29af0edef6814f12d74
mehdi gmira <mgmira@wiremind.fr>
2016-07-13 11:32:14 +0200

    test

--- gevent_channels.py
+++ gevent_channels.py
@@ -7,9 +7,8 @@ from gevent.queue import Queue
 
 
 class Channel:
-
     def __init__(self):
-        self.event = Event
+        self.event = Event()
         self.event.channel = self
         self.queue = Queue(maxsize=1)
 
@@ -20,6 +19,39 @@ class Channel:
 
 def channel_wait(channels, timeout=None):
     assert all(isinstance(channel, Channel) for channel in channels), "Can only wait on channels"
-    event = wait([channel.queue for channel in channels], timeout, 1)
+    event = wait([channel.event for channel in channels], timeout, 1)
     if event:
-        return event.channel.queue.get_no_wait()
+        val = event[0].channel.queue.get_nowait()
+        event[0].clear()
+        return val, event[0].channel
+
+
+if __name__ == "__main__":
+    from gevent import sleep, spawn, joinall
+
+    hey, hola = Channel(), Channel()
+
+
+    def g():
+        for i in range(100):
+            msg, channel = channel_wait([hey, hola])
+            if channel == hey:
+                print 'hey %s' % msg
+            else:
+                print 'hola %s' % msg
+            sleep(5)
+
+
+    def f():
+        for i in range(100):
+            if i % 3 == 1:
+                hey.put(i)
+                print 'put %s in hey' % i
+            else:
+                hola.put(i)
+                print 'put %s in hola' % i
+
+
+    g_f = spawn(f)
+    g_g = spawn(g)
+    joinall([g_f, g_g])
